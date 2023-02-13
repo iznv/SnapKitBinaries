6eaef6db06e2e6713b2d68098f0a9723d460bef4
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-02-13 13:36:20 +0300

    Add 5.6.0

--- Package.swift
+++ Package.swift
@@ -3,26 +3,21 @@
 
 import PackageDescription
 
+
+import PackageDescription
+
 let package = Package(
     name: "SnapKitBinaries",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SnapKitBinaries",
-            targets: ["SnapKitBinaries"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "SnapKit",
+            targets: [
+                "SnapKit"
+            ]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SnapKitBinaries",
-            dependencies: []),
-        .testTarget(
-            name: "SnapKitBinariesTests",
-            dependencies: ["SnapKitBinaries"]),
+        .binaryTarget(name: "SnapKit",
+                      path: "Frameworks/SnapKit.xcframework")
     ]
 )
